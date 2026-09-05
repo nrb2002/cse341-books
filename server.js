bedd859fbabc2714c5c6e2eab8549adc0cef156e
nrb2002 <tshibasubaron@yahoo.fr>
2026-09-05 11:34:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-
-if (!PORT) {
-    try {
-        throw new Error('PORT is not defined. Please set the PORT environment variable.');
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
-}
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
